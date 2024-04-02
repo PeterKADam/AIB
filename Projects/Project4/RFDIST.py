5db778efa14660310ce41a61b4917db394b96778
PeterKADam <peter.konrad.alling@gmail.com>
2024-04-02 14:50:51 +0200

    use touples of sets instead of touples of lists, so we dont need to sort in count_shared

--- Projects/Project4/RFDIST.py
+++ Projects/Project4/RFDIST.py
@@ -11,26 +11,26 @@ def loadtree(path):
 def findsplits(tree: BaseTree.Tree):
     def get_leaf_names(clade):
         if clade.is_terminal():  # base case: if the clade is a leaf
-            return [clade.name]
+            return {clade.name}
         else:  # recursive case: if the clade is an internal node
-            leaf_names = []
+            leaf_names = set()
             for child in clade.clades:
-                leaf_names.extend(get_leaf_names(child))
+                leaf_names.update(get_leaf_names(child))
         return leaf_names
 
-    all_leaves = set(get_leaf_names(tree.root))  # get all leaf names in the tree
+    all_leaves = get_leaf_names(tree.root)  # get all leaf names in the tree
     splits = []
     for clade in tree.find_clades(
         terminal=False
     ):  # iterate over all clades, not just direct children of root
         if not clade.is_terminal():
-            leaf_names = set(get_leaf_names(clade))  # get leaf names in this clade
+            leaf_names = get_leaf_names(clade)  # get leaf names in this clade
             other_leaves = all_leaves - leaf_names  # get leaf names not in this clade
             if (
                 leaf_names and other_leaves
             ):  # only add the split if neither side is empty
                 splits.append(
-                    (list(leaf_names), list(other_leaves))
+                    (leaf_names, other_leaves)
                 )  # add pair of splits to the list
     return splits
 
@@ -39,8 +39,7 @@ def count_shared(splits1, splits2):
     shared = 0
     for split1 in splits1:
         for split2 in splits2:
-            # dont do this for big sequences.  #fixlater convert to sets.
-            if tuple(sorted(split1)) == tuple(sorted(split2)):
+            if split1 == split2:
                 shared += 1
     return shared
 
