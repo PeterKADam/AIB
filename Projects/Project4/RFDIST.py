a78d24cefb4bcf3e4f91357671c0b89b60f2b5f2
PeterKADam <peter.konrad.alling@gmail.com>
2024-04-04 11:50:29 +0200

    did trees

--- Projects/Project4/RFDIST.py
+++ Projects/Project4/RFDIST.py
@@ -1,15 +1,16 @@
 from Bio import Phylo
 from Bio.Phylo import BaseTree
 
+from typing import List, Tuple, Set
 import sys
 
 
-def loadtree(path):
+def loadtree(path: str) -> BaseTree.Tree:
     return Phylo.read(path, "newick")
 
 
-def findsplits(tree: BaseTree.Tree):
-    def get_leaf_names(clade):
+def findsplits(tree: BaseTree.Tree) -> List[Tuple[Set[str], Set[str]]]:
+    def get_leaf_names(clade: BaseTree.Clade) -> Set[str]:
         if clade.is_terminal():  # base case: if the clade is a leaf
             return {clade.name}
         else:  # recursive case: if the clade is an internal node
@@ -35,21 +36,18 @@ def findsplits(tree: BaseTree.Tree):
     return splits
 
 
-def count_shared(splits1, splits2):
+def calcRFDist(
+    splits1: List[Tuple[Set[str], Set[str]]], splits2: List[Tuple[Set[str], Set[str]]]
+) -> int:
     shared = 0
     for split1 in splits1:
         for split2 in splits2:
             if split1 == split2:
                 shared += 1
-    return shared
-
-
-def calcRFDist(splits1, splits2):
-    shared = count_shared(splits1, splits2)
     return len(splits1) + len(splits2) - 2 * shared
 
 
-t1 = loadtree(sys.argv[1])
-t2 = loadtree(sys.argv[2])
+# t1 = loadtree(sys.argv[1])
+# t2 = loadtree(sys.argv[2])
 
-print(calcRFDist(findsplits(t1), findsplits(t2)))
+# print(calcRFDist(findsplits(t1), findsplits(t2)))
