bb8e7c76cb3dcbc68fd7b1d38d495f9576a65816
AneNaur <202008164@post.au.dk>
2024-04-16 13:21:02 +0200

    Hej

--- Projects/Project5/njtreemaker.py
+++ Projects/Project5/njtreemaker.py
@@ -2,8 +2,27 @@
 import
 
 
-def load_distancematrix():
-	return matrix
+def load_distancematrix(path):  
+	distance_matrix = []
+
+    with open(path, "r") as file:
+        for line_num, line in enumerate(file):
+            line = line.strip()
+            if line:
+                elements = line.split()
+                if line_num == 0:  # First line, indicating matrix size
+                    size = int(elements[0])
+                    labels = [""] + elements[1:]
+                    distance_matrix.append([size] + labels)
+                else:
+                    label = elements[0]
+                    distances = list(map(float, elements[1:]))
+                    new_line = [label] + distances
+                    distance_matrix.append(new_line)
+
+    return distance_matrix
+
+print(load_distancematrix("example_slide4.phy"))
 
 def build_root_tree(matrix):
 	return tree
