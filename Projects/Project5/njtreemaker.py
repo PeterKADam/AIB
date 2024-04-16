f9cff99adf50047b4576fadaff7fdb43bdab1847
PeterKADam <peter.konrad.alling@gmail.com>
2024-04-16 12:42:49 +0200

    start project5

---
+++ Projects/Project5/njtreemaker.py
@@ -0,0 +1,22 @@
+
+import
+
+
+def load_distancematrix():
+	return matrix
+
+def build_root_tree(matrix):
+	return tree
+
+def update_matrix(tree):
+	return matrix
+
+def add_node_to_tree(tree,something):
+	return tree
+
+def terminate(tree):
+	return tree
+
+def save_newick(tree):
+	return
+ 
