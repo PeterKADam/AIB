62a1b762c97cb89aa4094a8eda797c77110b0dc4
AneNaur <202008164@post.au.dk>
2024-04-16 13:30:01 +0200

    load_distancematrix

--- Projects/Project5/njtreemaker.py
+++ Projects/Project5/njtreemaker.py
@@ -5,49 +5,51 @@ from Bio.Phylo.BaseTree import Tree, Clade
 def load_distancematrix(path):  
 	distance_matrix = []
 
-    with open(path, "r") as file:
-        for line_num, line in enumerate(file):
-            line = line.strip()
-            if line:
-                elements = line.split()
-                if line_num == 0:  # First line, indicating matrix size
-                    size = int(elements[0])
-                    labels = [""] + elements[1:]
-                    distance_matrix.append([size] + labels)
-                else:
-                    label = elements[0]
-                    distances = list(map(float, elements[1:]))
-                    new_line = [label] + distances
-                    distance_matrix.append(new_line)
+	with open(path, "r") as file:
+		for line_num, line in enumerate(file):
+			line = line.strip()
+			if line:
+				elements = line.split()
+				if line_num == 0:  # First line, indicating matrix size
+					size = int(elements[0])
+					labels = elements[1:]
+					distance_matrix.append([size] + labels)
+				else:
+					label = elements[0]
+					distances = list(map(float, elements[1:]))
+					new_line = [label] + distances
+					distance_matrix[0].append(label)
+					distance_matrix.append(new_line)
 
-    return distance_matrix
+	return distance_matrix
 
 print(load_distancematrix("example_slide4.phy"))
+
 def load_distancematrix():
-    return matrix
+	return matrix
 
 
 def build_root_tree(matrix: list[list]) -> Tree:
 
-    clades = [Clade(None, name) for name in matrix[0][1:]]
+	clades = [Clade(None, name) for name in matrix[0][1:]]
 
-    tree = Tree(clades=clades, rooted=False)
+	tree = Tree(clades=clades, rooted=False)
 
-    return tree
+	return tree
 
 
 def update_matrix(tree: Tree) -> list[list]:
-    return matrix
+	return matrix
 
 
 def add_node_to_tree(tree: Tree, something) -> Tree:
-    return tree
+	return tree
 
 
 def terminate(tree) -> Tree:
-    return tree
+	return tree
 
 
 def save_newick(tree: BaseTree.Tree, filename: str):
-    Phylo.write(tree, filename, "newick")
-    return
+	Phylo.write(tree, filename, "newick")
+	return
